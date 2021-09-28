748feb4fde69696ce080d11266d0e9af8f367dda
Adhil muhammed <82211309+Adhil80@users.noreply.github.com>
2021-09-28 19:31:23 +0530

    Create linux_setup.sh

---
+++ linux_setup.sh
@@ -0,0 +1,30 @@
+sudo apt update
+sudo apt install git
+sudo apt install gnome-tweaks
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+cd WhiteSur-gtk-theme
+sudo ./install.sh
+sudo ./install.sh -c dark -c light
+sudo ./install.sh -s 220
+sudo ./install.sh -i fedora
+sudo ./tweaks.sh -f
+sudo add-apt-repository universe
+sudo apt install gnome-shell-extension-dashtodock
+sudo sudo ./tweaks.sh -g
+sudo ./tweaks.sh -g
+git clone https://github.com/vinceliuice/WhiteSur-kde.git
+cd WhiteSur-kde
+sudo ./install.sh
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+cd WhiteSur-icon-theme 
+sudo killall -3 gnome-shell
+git clone https://github.com/vinceliuice/WhiteSur-cursors.git
+cd WhiteSur-cursors 
+sudo ./install.sh
+sudo ./install.sh
+sudo snap install code --classic
+sudo snap install android-studio --classic
+sudo snap install pycharm-community --classic
+sudo apt install npm
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
